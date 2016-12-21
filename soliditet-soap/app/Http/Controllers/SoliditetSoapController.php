cf010aa56465d26ebc7a893e5834304aafd72475
tobysaunders <toby.saunders@mavad.co.uk>
2016-12-21 16:27:57 +0000

    Soliditet class

--- soliditet-soap/app/Http/Controllers/SoliditetSoapController.php
+++ soliditet-soap/app/Http/Controllers/SoliditetSoapController.php
@@ -11,33 +11,48 @@ use BeSimple\SoapClient\SoapRequest AS SoapRequest;
 class SoliditetSoapController extends BaseController
 {
     //
-    function getReport(){
-        echo 'here';
+    function getReport()
+    {
+        //echo 'here';
         //if($request->isMethod('post')){
-            $simpleClient = new SoapClient('https://webservice.soliditet.se/brg/services/NBKReportServiceSmall?wsdl');
+        $simpleClient = new SoapClient('https://webservice.soliditet.se/brg/services/NBKReportServiceSmall?wsdl', array('trace' => 1));
 
-            $identity = new \stdClass();
-            $identity->user = env('APP_USERNAME');
-            $identity->password = env('APP_PASSWORD');
+        $identity = new \stdClass();
+        $identity->user = env('APP_USERNAME');
+        $identity->password = env('APP_PASSWORD');
+        //print_r($identity);
 
-            $includeEmptyFields = new \SoapHeader('brg','includeEmptyFields','true', false);
-            $includeFieldDisplayName = new \SoapHeader('brg','includeFieldDisplayName','true', false);
-            $fieldDisplayNameLanguage = new \SoapHeader('brg','fieldDisplayNameLanguage','sv', false);
+        $includeEmptyFields = new \SoapHeader('brg', 'includeEmptyFields', 'true', false);
+        $includeFieldDisplayName = new \SoapHeader('brg', 'includeFieldDisplayName', 'true', false);
+        $fieldDisplayNameLanguage = new \SoapHeader('brg', 'fieldDisplayNameLanguage', 'sv', false);
+        //$userId = new \SoapHeader('brg','userId',env('APP_USERNAME'), false);
+        //$userPassword = new \SoapHeader('brg','userPassword',env('APP_PASSWORD'), false);
 
-            $simpleClient->__setSoapHeaders(array($includeEmptyFields,$includeFieldDisplayName,$fieldDisplayNameLanguage));
-
-
-            $nBKReportServiceSmallRequest = new \stdClass;
-            $nBKReportServiceSmallRequest->identity = $identity;
-
-            $nBKVar = new \SoapVar($nBKReportServiceSmallRequest, SOAP_ENC_OBJECT, 'nBKReportServiceSmallRequest');
-
-            return $simpleClient->__doRequest($nBKVar, 'https://webservice.soliditet.se/brg/services/NBKReportServiceSmall', 'request', '1' );
+        $simpleClient->__setSoapHeaders(array($includeEmptyFields, $includeFieldDisplayName, $fieldDisplayNameLanguage));
 
 
+        $nBKReportServiceSmallRequest = new \stdClass;
+        $nBKReportServiceSmallRequest->identity = $identity;
 
+        $nBKReportServiceSmallRequest->contract = new \stdClass;
 
+        $nBKReportServiceSmallRequest->reportCriteria = new \stdClass;
+        $nBKReportServiceSmallRequest->reportCriteria->startPos = 0;
+        $nBKReportServiceSmallRequest->reportCriteria->pageSize = 50;
+        $nBKReportServiceSmallRequest->reportCriteria->numberOfHits = 500;
+        $nBKReportServiceSmallRequest->reportCriteria->buyReport = false;
+        $nBKReportServiceSmallRequest->reportCriteria->language = 'SV';
 
-        }
-   // }
+        $nBKReportServiceSmallRequest->reportCriteria->countries = new \stdClass;
+        $nBKReportServiceSmallRequest->reportCriteria->countries->country = 'SE';
+
+        $nBKReportServiceSmallRequest->reportCriteria->criteria = new \stdClass;
+        $nBKReportServiceSmallRequest->reportCriteria->criteria->freetext = 'stockholm';
+
+        $response = $simpleClient->service($nBKReportServiceSmallRequest);
+
+        return var_dump($response);
+
+    }
+
 }
